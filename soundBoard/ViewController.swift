8e91573af052737b2df1c79d7499795fc9586e50
Cameron Krischel <ckrischel@he3labs.com>
2019-07-14 14:02:00 -0700

    Initial Commit

---
+++ soundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  soundBoard
+//
+//  Created by Cameron Krischel on 7/14/19.
+//  Copyright © 2019 Cameron Krischel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
